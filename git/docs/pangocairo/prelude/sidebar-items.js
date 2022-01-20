04160ca1661a653964c307d4f75e83f617f3909d
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2022-01-20 20:55:19 +0000

    deploy: 7776a2eca23a0a99539a4de0ff979ef4958ac096

--- git/docs/pangocairo/prelude/sidebar-items.js
+++ git/docs/pangocairo/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["functions",""]],"struct":[["Font","[`Font`][crate::Font] is an interface exported by fonts for use with Cairo."],["FontMap","[`FontMap`][crate::FontMap] is an interface exported by font maps for use with Cairo."]],"trait":[["FontMapExtManual",""]]});
+initSidebarItems({"trait":[["FontExt","Trait containing all [`struct@Font`] methods."],["FontMapExt","Trait containing all [`struct@FontMap`] methods."],["FontMapExtManual",""]]});
