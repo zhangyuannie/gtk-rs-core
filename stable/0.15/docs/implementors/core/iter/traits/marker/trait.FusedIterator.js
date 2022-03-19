f7b474a7969c5e7d71e3214d28bb84294c41ff88
sdroege <sdroege@users.noreply.github.com>
2022-03-19 09:57:00 +0000

    deploy: 2d23ae132cc76ec0086fd8ccdef23d18e7c2cef5

---
+++ stable/0.15/docs/implementors/core/iter/traits/marker/trait.FusedIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["glib"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"glib/struct.VariantTyIterator.html\" title=\"struct glib::VariantTyIterator\">VariantTyIterator</a>&lt;'a&gt;","synthetic":false,"types":["glib::variant_type::VariantTyIterator"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
